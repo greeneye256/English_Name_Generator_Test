0d709bcccc6b8921fc11d1fe2c2cc7da71173e86
Radu Simu <radusimu@gmail.com>
2019-10-19 16:06:49 +0300

    done

--- src/simedia/NameGenerator.java
+++ src/simedia/NameGenerator.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-class NameGenerator {
+public class NameGenerator {
 
-    private final static File fileBoyNames = new File("src/simedia/BoyNames");
-    private final static File fileGirlNames = new File("src/simedia/GirlNames");
-    private final static File fileLastNames = new File("src/simedia/LastNames");
+    private final static InputStream boys = NameGenerator.class.getResourceAsStream("/simedia/BoyNames");
+    private final static InputStream girls = NameGenerator.class.getResourceAsStream("/simedia/GirlNames");
+    private final static InputStream last = NameGenerator.class.getResourceAsStream("/simedia/LastNames");
     private static List<String> boyNames;
     private static List<String> girlNames;
     private static List<String> lastNames;
@@ -19,9 +19,9 @@ class NameGenerator {
     static {
         try {
 
-            boyNames = getArrayOfNamesFromCsv(fileBoyNames);
-            girlNames = getArrayOfNamesFromCsv(fileGirlNames);
-            lastNames = getArrayOfNamesFromCsv(fileLastNames);
+            boyNames = getArrayOfNamesFromCsv(boys);
+            girlNames = getArrayOfNamesFromCsv(girls);
+            lastNames = getArrayOfNamesFromCsv(last);
             firstNames.addAll(boyNames);
             firstNames.addAll(girlNames);
 
@@ -30,8 +30,8 @@ class NameGenerator {
         }
     }
 
-    private static List<String> getArrayOfNamesFromCsv(File file) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+    private static List<String> getArrayOfNamesFromCsv(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
         List<String> names = new ArrayList<>();
         String st;
         while ((st = bufferedReader.readLine()) != null) {
@@ -42,24 +42,24 @@ class NameGenerator {
         return names;
     }
 
-    static String getRandomBoyFirstName() {
+    public static String getRandomBoyFirstName() {
 
         return boyNames.get(new Random().nextInt(boyNames.size()));
     }
 
-    static String getRandomGirlFirstName() {
+    public static String getRandomGirlFirstName() {
         return girlNames.get(new Random().nextInt(girlNames.size()));
     }
 
-    static String getRandomLastName() {
+    public static String getRandomLastName() {
         return lastNames.get(new Random().nextInt(lastNames.size()));
     }
 
-    static String getRandomFirstName() {
+    public static String getRandomFirstName() {
         return firstNames.get(new Random().nextInt(firstNames.size()));
     }
 
-    static String getGenderFromFirstName(String firstName) {
+    public static String getGenderFromFirstName(String firstName) {
         if (boyNames.contains(firstName)) {
             return "m";
         }
@@ -69,7 +69,7 @@ class NameGenerator {
         return "no gender";
     }
 
-    static String getRandomFullName(){
+    public static String getRandomFullName(){
         return getRandomFirstName() + " " + getRandomLastName();
     }
 }
